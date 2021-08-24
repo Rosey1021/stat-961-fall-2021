1bc2e4c00f662e7775e9e5ebc7ebf734eb64672f
ekatsevi <katsevich.gene@gmail.com>
2021-08-24 17:04:51 -0400

    Added HW1 and preparing-reports

---
+++ data/download_data.R
@@ -0,0 +1,12 @@
+textbook_site = "http://users.stat.ufl.edu/~aa/glm/data"
+
+filename = "Anorexia.dat"
+destination = sprintf("data/%s", filename)
+if(!file.exists(destination)){
+  cat(sprintf("Downloading %s...\n", filename))
+  source = sprintf("%s/%s", textbook_site, filename)
+  anorexia_data = suppressWarnings(read_table(source, col_types = "ifdd-", skip = 5))
+  write_tsv(anorexia_data, file = destination)
+} else{
+  cat(sprintf("%s already exists.\n", filename))
+}
